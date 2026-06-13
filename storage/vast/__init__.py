f9a8343f2443ee6fe6b9430f9099db5af9800322
Chris Barnett <Christopher.Barnett@tufts.edu>
2026-06-13 00:24:23 -0400

    refactor course share creation to match project share construction

--- storage/vast/__init__.py
+++ storage/vast/__init__.py
@@ -1,9 +1,13 @@
 from .vast import get_quotas_batch, get_quota, get_all_quotas, set_quota, create_share
+from .course import create_course_share
+from .project import create_project_share
 
 __all__ = [
     "get_quotas_batch",
     "get_quota",
     "get_all_quotas",
     "set_quota",
-    "create_share"
+    "create_share",
+    "create_course_share",
+    "create_project_share"
 ]
--- storage/vast/course.py
+++ storage/vast/course.py
@@ -1,10 +1,12 @@
 import logging
 import shutil
 from pathlib import Path
-from .vast import create_share
+from coldfront.core.allocation.models import Allocation
+from coldfront_utils import update_allocation_attribute_value
+from .vast import create_share, native_path_to_cluster_path
+from storage.constants import SHARE_CREATION_STATE_ATTRIBUTE_NAME
 from storage.shell_utils import (check_group_exists, check_user_exists, 
-                                 validate_dirname, validate_groupname, 
-                                 validate_username, create_subfolder)
+                                 validate_dirname, create_subfolder)
 
 logger = logging.getLogger(__name__)
 
@@ -12,44 +14,43 @@ def create_course_share(native_path: str, quota_bytes: int, owner: str, group: s
     create_share(native_path=native_path.lower(), quota_bytes=quota_bytes, 
                  owner=owner, group=group, client_config_id=client_config_id, 
                  allocation_pk=allocation_pk)
-
-    owner = owner.strip().lower()
-    group = group.strip()
-    logger.info("checking user and group...")
-    
     try:
+        # reset state to pending
+        allocation = Allocation.objects.get(id=allocation_pk)
+        update_allocation_attribute_value(allocation, SHARE_CREATION_STATE_ATTRIBUTE_NAME, 'pending')
+        owner = owner.strip().lower()
+        group = group.strip()
+        logger.info("checking user and group...")
         check_user_exists(owner)
-    except ValueError as e:
-        logger.error(f"User {owner} is not on the system. Please fix account issues and try again: {e}")
-        raise e
-    admin_group = f'{group}admin'
-    try:
         check_group_exists(group)
+        admin_group = f'{group}admin'
         check_group_exists(admin_group)
-    except ValueError as e:
-        logger.error(f"Group {group} or {admin_group} is not on the system. Please fix group issues and try again: {e}")
-        raise e
 
-    return create_course_folder(course_name=native_path, owner=owner, group=group, admin_group=admin_group)
+        cluster_path = native_path_to_cluster_path(native_path, client_config_id=client_config_id)
+        create_course_folder(cluster_path=Path(cluster_path), owner=owner, group=group, admin_group=admin_group)
+    except Exception as e:
+        logger.error(f"Error creating course share for path {native_path} in VAST: {e}")
+        update_allocation_attribute_value(allocation, SHARE_CREATION_STATE_ATTRIBUTE_NAME, 'failed')
+        raise e 
 
 
-def create_course_folder(course_name, owner, group, admin_group):
-    validate_dirname(course_name)
+def create_course_folder(cluster_path: Path, owner: str, group: str, admin_group: str):
+    validate_dirname(cluster_path.name)
     # owner is the username
-    project_path = Path(f'/cluster/tufts/{course_name}')
-    if project_path.exists():
-        if project_path.owner() != owner or project_path.group() != group:
-            shutil.chown(project_path, owner, group)
-        if oct(project_path.stat().st_mode & 0o7777) != oct(0o2750):
-            project_path.chmod(0o2750)
-        create_subfolder(project_path, 'shared', owner, admin_group, mode=0o2775)
-        shared_path = project_path / 'shared'
+
+    if cluster_path.exists():
+        if cluster_path.owner() != owner or cluster_path.group() != group:
+            shutil.chown(cluster_path, owner, group)
+        if oct(cluster_path.stat().st_mode & 0o7777) != oct(0o2750):
+            cluster_path.chmod(0o2750)
+        create_subfolder(cluster_path, 'shared', owner, admin_group, mode=0o2775)
+        shared_path = cluster_path / 'shared'
         if shared_path.exists():
             if shared_path.owner() != owner or shared_path.group() != admin_group:
                 shutil.chown(shared_path, owner, admin_group)
             if oct(shared_path.stat().st_mode & 0o7777) != oct(0o2775):
                 shared_path.chmod(0o2775)
         # course owner folder
-        create_subfolder(project_path, owner, owner, admin_group)
+        create_subfolder(cluster_path, owner, owner, admin_group)
         return True
     return False
